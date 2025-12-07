3fe54a6ae31e0a3f8bbd926b979fe5484bbeca2f
jhon dieter mayta flores <MFJ3005600@est.univalle.edu>
2025-12-07 18:23:25 -0400

    planB/usando otro metodo de deployment v2

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,56 +1,36 @@
 variable "tenancy_id" {
-  type        = string
-  description = "OCID del tenancy"
+  type = string
 }
 
 variable "compartment_id" {
-  type        = string
-  description = "OCID del compartment donde crear OKE"
+  type = string
 }
 
 variable "region" {
-  type        = string
-  description = "Región OCI, ej: sa-saopaulo-1"
+  type = string
 }
-
-variable "home_region" {
-  type        = string
-  description = "Región home de tu tenancy"
-}
-
 
 variable "label_prefix" {
-  type        = string
-  description = "Prefijo para recursos"
+  type = string
 }
 
-
-
-# Autenticación para OCI (API signing key)
 variable "user_id" {
-  type        = string
-  description = "OCID del usuario que autentica contra OCI"
+  type = string
 }
 
 variable "api_fingerprint" {
-  type        = string
-  description = "Fingerprint de la API signing key"
+  type = string
 }
-
 
 variable "api_private_key" {
-  type        = string
-  description = "Contenido PEM de la API signing key"
-  sensitive   = true
+  type     = string
+  sensitive = true
 }
 
-# Rutas donde Terraform encontrará las claves SSH (las crea el workflow)
 variable "ssh_public_key_path" {
-  type        = string
-  description = "Ruta al archivo de clave pública SSH para nodos OKE"
+  type = string
 }
 
 variable "ssh_private_key_path" {
-  type        = string
-  description = "Ruta al archivo de clave privada SSH para nodos OKE"
+  type = string
 }
