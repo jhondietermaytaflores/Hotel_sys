7f37c6290799285608bc8216d83e8728ae417deb
jhon dieter mayta flores <MFJ3005600@est.univalle.edu>
2025-12-06 23:37:22 -0400

    fix/edit outputs.tf para el deploy v1

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,9 +1,12 @@
+# -------------------------------------------------------------------
+# OUTPUTS
+# -------------------------------------------------------------------
+
 output "cluster_id" {
-  description = "OCID del cluster OKE"
-  value       = module.oke.cluster_id
+  value = oci_containerengine_cluster.oke.id
 }
 
 output "kubeconfig_command" {
-  description = "Comando sugerido para generar kubeconfig vía OCI CLI"
-  value       = "oci ce cluster create-kubeconfig --cluster-id ${module.oke.cluster_id} --file $HOME/.kube/config --region ${var.region} --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT"
+  description = "Comando para generar kubeconfig con OCI CLI"
+  value       = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.oke.id} --file $HOME/.kube/config --region ${var.region} --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT"
 }
